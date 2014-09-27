3f7252cf891d1d6affde38d265f5b4a7fe22b361
SpawnABL <barriosarturo@yahoo.com.mx>
2014-09-27 13:02:55 -0500

    Agregando Archivos
    Se creo el CSS de Comentarios y Conexion.Class con conexion a Servidor !

--- clases/conexion.class.php
+++ clases/conexion.class.php
@@ -2,7 +2,7 @@
 class conexion{
 	//creamos una funcion (metodo) que se llama igual a la clase
 	//para definir el comportamiento del objeto en su inicializacion
-	function conexion($host='localhost',$user='root',$password='', $database='artet0_diplomado'){ //metodo constructor nombre igual a la clase
+	function conexion($host='phpmyadmin.itdurango.mx',$user='artet0_diplomado',$password='d1pl0m4d0', $database='artet0_diplomado'){ //metodo constructor nombre igual a la clase
 		$this->host = $host;
 		$this->user = $user;
 		$this->password = $password;
